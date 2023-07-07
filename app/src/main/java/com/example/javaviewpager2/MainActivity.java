f2a7a21aabac719b9f2877b684f39af187e3e046
Johnny Huang <a0900181636@gmail.com>
2023-07-07 16:47:47 +0800

    Topic: Make partial menu item can switch to their animation Content: 1.ViewPager - (1)Add basic animation to regular item. (2)Add preview animation to  one_screen_three_page_basic item. (3)Add preview and zoom animation to one_screen_three_page_advanced item. 2.PageIndicator - (1)Add PageIndicator to page_indicator item,but it can't turn off now.

--- app/src/main/java/com/example/javaviewpager2/MainActivity.java
+++ app/src/main/java/com/example/javaviewpager2/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toolbar;
@@ -38,23 +39,70 @@ public class MainActivity extends AppCompatActivity {
         // 將清單放入ListAdapter，並將ListAdapter綁定到viewPager2上。
         MyListAdapter adapter = new MyListAdapter(getPersonList());
         viewPager2.setAdapter(adapter);
+        setViewPagerTransformerEnlargeWhenScroll(0f, 40);
+        Log.d("johnny", "onCreate");
 
-        // 設置viewPager動畫效果
-        setViewPagerScroll();
-        setViewPagerTransformerEnlargeWhenScroll(0.2f,40);
+        // 以下兩行註解為初始化之後，依照選單的項目進行特效轉換。
+        // onCreate為初始化效果，接下來的特效變化由Menu中被選中的Item來決定。
+        // onCreateOptionsMenu(匯入自定義清單) --> onOptionsItemSelected(根據被選中的Item進行操作)
 
-        // 將PageIndicator與ViewPager2結合(attach)
-        linkPageIndicatorAndViewPager2();
+    }
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        //ButterKnife.bind(this);
+    // 將menu.xml的Layout與Item匯入activity_main.xml
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    // 獲得menu上不同Item的Id，並將對應的效果放到activity_main.xml。
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // 根據不同id產生不同的動效
+        // 以下的Log用來查驗拿到的Id是否正確
+        switch (id) {
+            // 基本的頁面切換，不能預覽(Clip = true)，也沒有放大效果(zoom = 0)。
+            case R.id.regular:
+                Log.d("johnny", "regular");
+                setViewPagerScroll(true); // 參數為Clip，true表示要裁減被Padding到的頁面。
+                setViewPagerTransformerEnlargeWhenScroll(0, 40);
+                return true;
+
+            // 基本的一屏三頁特效，能預覽(Clip = false)，沒有放大效果(zoom = 0)。
+            case R.id.one_screen_three_page_basic:
+                Log.d("johnny", "one_screen_three_page_basic");
+                setViewPagerScroll(false);
+                setViewPagerTransformerEnlargeWhenScroll(0, 40);
+                return true;
+
+            // 進階的一屏三頁特效，能預覽(Clip = false)，有放大效果(zoom = 0.35f)。
+            case R.id.one_screen_three_page_advanced:
+                Log.d("johnny", "one_screen_three_page_advanced");
+                setViewPagerScroll(false);
+                setViewPagerTransformerEnlargeWhenScroll(0.35f, 40);
+                return true;
+
+            // 將PageIndicator放到activity_main.xml
+            case R.id.page_indicator:
+                Log.d("johnny", "page_indicator");
+                linkPageIndicatorAndViewPager2();
+                return true;
+
+            case R.id.style_move:
+                Log.d("johnny", "style_move");
+                return true;
+
+            case R.id.style_cube:
+                Log.d("johnny", "style_cube");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private List<MyPerson> getPersonList() {
         // create list
-        List<MyPerson> myList= new ArrayList<>();
+        List<MyPerson> myList = new ArrayList<>();
 
         // create fake person
         MyPerson myPerson1 = new MyPerson("菊草葉", "草", 14, R.drawable.green);
@@ -69,11 +117,11 @@ public class MainActivity extends AppCompatActivity {
         return myList;
     }
 
-    private void setViewPagerScroll() {
+    private void setViewPagerScroll(Boolean clip) {
         // 設置一屏三頁的效果
         viewPager2.setOffscreenPageLimit(1); // 設置為1，會載入左右各一頁，避免使用者滑動過快來不及加載。
-        viewPager2.setClipToPadding(false); // 保留viewPager2視窗被padding的地方
-        viewPager2.setClipChildren(false); // 保留左右兩頁的視窗
+        viewPager2.setClipToPadding(clip); // 保留viewPager2視窗被padding的地方
+        viewPager2.setClipChildren(clip); // 保留左右兩頁的視窗
         // 讓首頁跟末頁沒有滑動效果
         viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
     }
@@ -118,31 +166,13 @@ public class MainActivity extends AppCompatActivity {
         // 每個被產生的tab在TabLayout裡會以水平方式排列，並根據在activity_main.xml的設定在背景放置對應顏色的圓點。
         // 綜合上述兩個註解，可看出這個函式就是產生PageIndicator的原因。
         new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+
             @Override
-            public void onConfigureTab( TabLayout.Tab tab, int position) {
+            public void onConfigureTab(TabLayout.Tab tab, int position) {
+
                 // Some implementation
             }
         }).attach();
-    }
 
-    @SuppressLint("ResourceType")
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        MainFragment f = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.style_cube); // style_cube記的做更改
-        switch (id) {
-            case R.id.style_move:
-                //f.setAnimationStyle(MainFragment.Move);
-                return true;
-            case R.id.style_cube:
-                //f.setAnimationStyle(MainFragment.Cube);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/javaviewpager2/MainFragment.java
+++ app/src/main/java/com/example/javaviewpager2/MainFragment.java
@@ -67,5 +67,4 @@ public class MainFragment extends Fragment {
 
  }
 
-}
 
